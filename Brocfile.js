42733d9d6214ef3d0ac15c142b4a415dd983185c
James-Mac <james@aehr.org>
2014-10-08 12:43:41 -0600

    Update to ember cli 0.1.1

--- Brocfile.js
+++ Brocfile.js
@@ -5,16 +5,16 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 /* Vendor CSS */
-app.import('vendor/font-awesome/css/font-awesome.css');  // Font Awesome Icons
-app.import('vendor/animate.css/animate.css');            // Animate.css
-app.import('vendor/nprogress/nprogress.css');            // NProgess CSS
-app.import('vendor/vis/dist/vis.css');                   // vis CSS
+app.import('bower_components/font-awesome/css/font-awesome.css');  // Font Awesome Icons
+app.import('bower_components/animate.css/animate.css');            // Animate.css
+app.import('bower_components/nprogress/nprogress.css');            // NProgess CSS
+app.import('bower_components/vis/dist/vis.css');                   // vis CSS
 
 /* Vendor JS */
-app.import('vendor/bootstrap/dist/js/bootstrap.min.js'); // Bootstrap JS
-app.import('vendor/moment/moment.js');                   // Moment.js
-app.import('vendor/bootstrap.growl/bootstrap-growl.js'); // Bootstrap Growl Notifications JS
-app.import('vendor/nprogress/nprogress.js');             // NProgress JS
-app.import('vendor/vis/dist/vis.js');                    // vis JS
+app.import('bower_components/bootstrap/dist/js/bootstrap.min.js'); // Bootstrap JS
+app.import('bower_components/moment/moment.js');                   // Moment.js
+app.import('bower_components/bootstrap.growl/bootstrap-growl.js'); // Bootstrap Growl Notifications JS
+app.import('bower_components/nprogress/nprogress.js');             // NProgress JS
+app.import('bower_components/vis/dist/vis.js');                    // vis JS
 
 module.exports = app.toTree();
--- tests/helpers/resolver.js
+++ tests/helpers/resolver.js
@@ -1,9 +1,11 @@
 import Resolver from 'ember/resolver';
+import config from '../../config/environment';
 
 var resolver = Resolver.create();
 
 resolver.namespace = {
-  modulePrefix: 'trust-admin'
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix
 };
 
 export default resolver;
