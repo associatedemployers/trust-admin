4a762bff867777b666b99cc640fe75842bfeb8fe
James-Mac <james@aehr.org>
2014-10-08 11:46:27 -0600

    New dependencies

--- Brocfile.js
+++ Brocfile.js
@@ -5,12 +5,16 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 /* Vendor CSS */
-app.import('vendor/font-awesome/css/font-awesome.css');
-app.import('vendor/animate.css/animate.css');
+app.import('vendor/font-awesome/css/font-awesome.css');  // Font Awesome Icons
+app.import('vendor/animate.css/animate.css');            // Animate.css
+app.import('vendor/nprogress/nprogress.css');            // NProgess CSS
+app.import('vendor/vis/dist/vis.css');                   // vis CSS
 
 /* Vendor JS */
-app.import('vendor/bootstrap/dist/js/bootstrap.min.js');
-app.import('vendor/moment/moment.js');
-app.import('vendor/bootstrap.growl/bootstrap-growl.js');
+app.import('vendor/bootstrap/dist/js/bootstrap.min.js'); // Bootstrap JS
+app.import('vendor/moment/moment.js');                   // Moment.js
+app.import('vendor/bootstrap.growl/bootstrap-growl.js'); // Bootstrap Growl Notifications JS
+app.import('vendor/nprogress/nprogress.js');             // NProgress JS
+app.import('vendor/vis/dist/vis.js');                    // vis JS
 
 module.exports = app.toTree();
