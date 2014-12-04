365a3eb0bd326e51cc4be1a2ba23428204f6aafe
James-Mac <james@aehr.org>
2014-12-04 14:15:19 -0700

    Parameteritize company metric display, + filters for companies

--- app/controllers/companies.js
+++ app/controllers/companies.js
@@ -3,6 +3,12 @@ import ResourcePaginatorMixin from 'trust-admin/mixins/resource-paginator';
 import ResourceFiltersMixin from 'trust-admin/mixins/resource-filters';
 import Company from 'trust-admin/models/company';
 
+var filters = {
+  city: '',
+  state: null,
+  minEmployees: null,
+  maxEmployees: null
+};
 
 export default Ember.ArrayController.extend(ResourcePaginatorMixin, ResourceFiltersMixin, {
   needs: [ 'application' ],
@@ -10,8 +16,49 @@ export default Ember.ArrayController.extend(ResourcePaginatorMixin, ResourceFilt
   queryParams: [
     'sort',
     'page',
-    'itemsPerPage'
+    'itemsPerPage',
+    'serializeFilters.city',
+    'serializeFilters.state',
+    'serializeFilters.minEmployees',
+    'serializeFilters.maxEmployees'
   ],
+
+  filterMap: {
+    city: 'address.city',
+    state: 'address.state'
+  },
+
+  filterValueNormalization: {
+    minEmployees: function ( value ) {
+      if( !value ) {
+        return null;
+      }
+
+      var absVal = Math.abs( value ) - 1;
+
+      return {
+        newKey: 'employees.' + absVal,
+        value: 'exists'
+      };
+    },
+
+    maxEmployees: function ( value ) {
+      if( !value ) {
+        return null;
+      }
+
+      var absVal = Math.abs( value );
+
+      return {
+        newKey: 'employees.' + absVal,
+        value: 'nexists'
+      };
+    }
+  },
+
+  // Filter Defaults
+  serializeFilters: $.extend({}, filters),
+  filterDefaults:   $.extend({}, filters),
 
   // Defaults
   sort:         'ASC',
@@ -22,6 +69,26 @@ export default Ember.ArrayController.extend(ResourcePaginatorMixin, ResourceFilt
   itemsPerPageOptions: [ 10, 20, 40, 60 ],
   resource: Company,
   modelName: 'Company',
+
+  _receivedFilterInput: function () {
+    this.set('filtersApplied', false);
+  }.observes(
+    'serializeFilters.city',
+    'serializeFilters.state',
+    'serializeFilters.minEmployees',
+    'serializeFilters.maxEmployees'
+  ),
+
+  cities: function () {
+    var self = this;
+
+    Ember.$.getJSON('/api/companies/', {
+      _distinct: true,
+      select:    'address.city'
+    }).then(function ( res ) {
+      self.set('cities', res);
+    });
+  }.property(),
 
   actions: {
     toggleProperty: function ( prop ) {
--- app/controllers/company/metrics.js
+++ app/controllers/company/metrics.js
@@ -1,11 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: [ 'show' ],
   hasSelectedMetric: false,
+
   metrics: [
     'Employees Over Time',
     'Plan Participation'
   ],
+
+  shownMetricDidChange: function () {
+    var metricName = this.get('show'),
+        metrics    = this.get('metricTypes'),
+        self       = this;
+
+    this.set('hasSelectedMetric', false);
+
+    if( !metricName ) {
+      return;
+    }
+
+    var overrideMetrics = metrics.map(function ( metric ) {
+      var selected = metric.name === metricName;
+
+      self.set('showing' + metric.name.replace(/\s/g, ''), selected);
+
+      metric.active = selected;
+      return metric;
+    });
+
+    this.setProperties({
+      metricTypes: overrideMetrics,
+      hasSelectedMetric: true
+    });
+  }.observes('show'),
 
   metricTypes: function () {
     return this.get('metrics').map(function ( metric ) {
@@ -17,18 +45,7 @@ export default Ember.Controller.extend({
 
   actions: {
     showMetric: function ( metricName ) {
-      this.set('hasSelectedMetric', true);
-
-      var metrics = this.get('metricTypes'),
-          self    = this;
-
-      this.set('metricTypes', metrics.map(function ( metric ) {
-        var selected = metric.name === metricName;
-
-        self.set('showing' + metric.name.replace(/\s/g, ''), selected);
-        metric.active = selected;
-        return metric;
-      }));
+      this.set('show', metricName);
     }
   }
 });
