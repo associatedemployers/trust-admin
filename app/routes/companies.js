8e9ced9c292bfcca9a84ecf8657a0d4b9c6dca62
James-Mac <james@aehr.org>
2014-12-04 14:13:36 -0700

    GH bug

---
+++ app/routes/companies.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import LoadableRouteMixin from 'trust-admin/mixins/loadable-route';
+
+export default Ember.Route.extend(LoadableRouteMixin, {
+  model: function ( params ) {
+    var query = {
+      limit: params.itemsPerPage, page: params.page - 1
+    };
+
+    if( params.filtersApplied && this.get('controller') && typeof this.get('controller.filters') === 'object' ) {
+      $.extend( query, this.get('controller.filters') );
+    }
+
+    return this.store.find('company', query);
+  }
+});
