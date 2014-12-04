8f992905b5d2f6ebf4a71f31ce58a160225f5b80
James-Mac <james@aehr.org>
2014-12-04 14:13:11 -0700

    Fix applying filters on forward/back navigation

--- app/routes/companies.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import LoadableRouteMixin from 'trust-admin/mixins/loadable-route';
-
-export default Ember.Route.extend(LoadableRouteMixin, {
-  model: function ( params ) {
-    return this.store.find('company', { limit: params.itemsPerPage, page: params.page - 1 });
-  }
-});
--- app/routes/employees.js
+++ app/routes/employees.js
@@ -3,6 +3,14 @@ import LoadableRouteMixin from 'trust-admin/mixins/loadable-route';
 
 export default Ember.Route.extend(LoadableRouteMixin, {
   model: function ( params ) {
-    return this.store.find('employee', { limit: params.itemsPerPage, page: params.page - 1 });
+    var query = {
+      limit: params.itemsPerPage, page: params.page - 1
+    };
+
+    if( params.filtersApplied && this.get('controller') && typeof this.get('controller.filters') === 'object' ) {
+      $.extend( query, this.get('controller.filters') );
+    }
+
+    return this.store.find('employee', query);
   }
 });
