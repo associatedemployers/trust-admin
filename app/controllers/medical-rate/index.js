cd5d6a6765d4dc30ae719b0945183ae44ba49d85
James-Mac <james@aehr.org>
2015-01-08 15:56:04 -0700

    Medical rate index view

---
+++ app/controllers/medical-rate/index.js
@@ -0,0 +1,73 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  // Rate Chart
+  rateChart: {
+    chartOptions: {
+      axis: {
+        x: {
+          type:       'category',
+          categories: []
+        }
+      },
+      legend: {
+        show: false
+      }
+    },
+    dataOptions: {
+      type:   'bar',
+      labels: {
+        format: function ( v ) {
+          return '$' + v.toFixed(2);
+        }
+      }
+    }
+  },
+
+  rateDataset: function () {
+    var dataset = Ember.A([
+      [ 'Rate' ]
+    ]);
+
+    var rates      = this.get('content').getProperties('employee', 'employeeAndSpouse', 'employeeAndChildren', 'family'),
+        categories = this.get('rateChart.chartOptions.axis.x.categories');
+
+    for ( var k in rates ) {
+      var decamelized = Ember.String.decamelize( k ).replace(/_/g, ' ');
+
+      categories.push( Ember.String.capitalize( decamelized ) );
+      dataset[ 0 ].push( rates[ k ] );
+    }
+
+    return dataset;
+  }.property(
+    'content.employee',
+    'content.employeeAndSpouse',
+    'content.employeeAndChildren',
+    'content.family'
+  ),
+
+  // Co-insurance Chart
+  coinsuranceChart: {
+    chartOptions: {},
+    dataOptions: {
+      type: 'donut'
+    }
+  },
+
+  coinsuranceDataset: function () {
+    var coinsurance = this.get('content.coInsurance'),
+        sCoin       = ( coinsurance ) ? coinsurance.split('/') : [ 0, 0 ];
+
+    return Ember.A([
+      [ 'Plan', sCoin[ 0 ] ],
+      [ 'Insured', sCoin[ 1 ] ]
+    ]);
+  }.property('coInsurance'),
+
+  actions: {
+    toggleProperty: function ( prop ) {
+      this.toggleProperty( prop );
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -8,13 +8,13 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('employees');
   this.resource('employee', { path: 'employees/:id' }, function () {
-    this.route('index', { path: '/' });
+    this.route('index', { path: '/summary' });
     this.route('edit');
   });
 
   this.route('companies');
   this.resource('company', { path: 'companies/:id' }, function () {
-    this.route('index', { path: '/' });
+    this.route('index', { path: '/summary' });
     this.route('edit');  
     this.route('employees');
     this.route('metrics');
@@ -26,25 +26,25 @@ Router.map(function() {
   });
 
   this.resource('user', { path: 'user/:id' }, function () {
-    this.route('index', { path: '/' });
+    this.route('index', { path: '/summary' });
     this.route('edit', { path: '/edit' });
   });
 
   this.route('medical-rates');
   this.resource('medical-rate', { path: 'medical-rate/:id' }, function () {
-    this.route('index', { path: '/' });
+    this.route('index', { path: '/summary' });
     this.route('edit');
   });
 
   this.route('search');
   this.resource('metrics', {}, function () {
-    this.route('index', { path: '/' });
+    this.route('index', { path: '/summary' });
     this.route('charts');
   });
 
   // Nested Resources
   this.resource('employee.dependent', { path: 'employees/:employeeid/dependents/:dependentid' }, function () {
-    this.route('index', { path: '/' });
+    this.route('index', { path: '/summary' });
     this.route('edit');
   });
 
