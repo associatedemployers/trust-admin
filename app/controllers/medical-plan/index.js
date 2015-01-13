9cebe5d9928ae89ee96c6d64f2323421b5713855
James-Mac <james@aehr.org>
2015-01-13 13:57:34 -0700

    Finish up participation data visualizations

--- app/controllers/medical-plan/index.js
+++ app/controllers/medical-plan/index.js
@@ -2,7 +2,36 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   rateCounts: {},
+  participationCounts: {},
+
   ratesChart: {
+    chartOptions: {
+      color: {
+        pattern: [ '#c5c5c5', '#00e075' ]
+      }
+    },
+    dataOptions: {
+      type: 'donut'
+    }
+  },
+
+  participationChart: {
+    chartOptions: {
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      }
+    },
+    dataOptions: {
+      x: 'x'
+    }
+  },
+
+  participationPieChart: {
     chartOptions: {
       color: {
         pattern: [ '#c5c5c5', '#00e075' ]
@@ -38,6 +67,43 @@ export default Ember.ObjectController.extend({
     }).fail( handleError );
   }.observes('showRates'),
 
+  _getParticipationCounts: function () {
+    if ( !this.get('showParticipation') ) {
+      return;
+    }
+
+    var self = this,
+        query = {
+          _count: true,
+          medicalPlan: this.get('content.id'),
+          legacyClientTerminationDate: 'nexists'
+        },
+        _counts = {};
+
+    var handleError = function ( err ) {
+      console.error( err );
+    };
+
+    Ember.$.getJSON('/api/employees', query).then(function ( res ) {
+      if ( !res || isNaN(res.count) ) {
+        return handleError('Invalid server response.');
+      }
+
+      _counts.assigned = res.count;
+      delete query.medicalPlan;
+
+      return Ember.$.getJSON('/api/employees', query);
+    }).then(function ( res ) {
+      if ( !res || isNaN(res.count) ) {
+        return handleError('Invalid server response.');
+      }
+
+      _counts.total = res.count;
+
+      self.set('participationCounts', _counts);
+    }).fail( handleError );
+  }.observes('showParticipation'),
+
   _getParticipationDataset: function () {
     if ( !this.get('showParticipation') ) {
       return;
@@ -69,14 +135,19 @@ export default Ember.ObjectController.extend({
       loop.add(Math.ceil( monthsBetween / timePad ), 'months');
     }
 
+    var currentMonth = moment( currentDate ).date( 1 ).format('YYYY-MM-DD');
+
+    if ( dataset[ 0 ][ dataset[ 0 ].length - 1 ] !== currentMonth ) {
+      dataset[ 0 ].pushObject( currentMonth );
+    }
+
     Ember.RSVP.all(dataset[ 0 ].map(function ( date ) {
       var jsDate = moment(date, 'YYYY-MM-DD').toDate();
 
       return new Ember.RSVP.Promise(function ( resolve, reject ) {
-        console.log('getting');
         Ember.$.getJSON('/api/employees', {
           _count: true,
-          plan: self.get('content.id'),
+          medicalPlan: self.get('content.id'),
           legacyClientEmploymentDate: {
             $lt: jsDate
           },
@@ -94,7 +165,6 @@ export default Ember.ObjectController.extend({
             }
           ]
         }).then(function ( res ) {
-          console.log(res);
           if ( !res || isNaN(res.count) ) {
             return reject('Invalid server response.');
           }
@@ -104,27 +174,10 @@ export default Ember.ObjectController.extend({
       });
     }))
     .then(function ( counts ) {
-      self.set('participationDataset', dataset[ 1 ].concat( counts ));
+      dataset[ 1 ] = dataset[ 1 ].concat( counts );
+      self.set('participationDataset', dataset);
     })
-    .catch(function ( err ) {
-      console.error( err );
-    });
-
-
-
-
-    Ember.$.getJSON('/api/medical-rates', { _count: true, plan: this.get('content.id') }).then(function ( res ) {
-      if ( !res || isNaN(res.count) || isNaN(res.total) ) {
-        return handleError('Invalid server response.');
-      }
-
-      self.setProperties({
-        rateCounts: {
-          assigned: res.count,
-          total:    res.total
-        }
-      });
-    }).fail( handleError );
+    .catch( handleError );
   }.observes('showParticipation'),
 
   ratesDataset: function () {
@@ -139,6 +192,19 @@ export default Ember.ObjectController.extend({
       [ 'Assigned To Plan', rateCounts.assigned ]
     ]);
   }.property('rateCounts.assigned', 'rateCounts.total'),
+
+  participationPieDataset: function () {
+    var participationCounts = this.get('participationCounts');
+
+    if ( isNaN(participationCounts.total) || isNaN(participationCounts.assigned) ) {
+      return;
+    }
+
+    return Ember.A([
+      [ 'All Employees', participationCounts.total ],
+      [ 'Assigned To Plan', participationCounts.assigned ]
+    ]);
+  }.property('participationCounts.assigned', 'participationCounts.total'),
 
   actions: {
     toggleProperty: function ( prop ) {
